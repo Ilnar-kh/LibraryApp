f6894a90a24099e757f18bbf2c3cb0696ff25a6b
Ilnar Khusainov <ilnarkhusainov@MacBook-Air-Ilnar.local>
2026-03-18 23:31:05 +0500

    user1

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package repository;
 
+import db.DatabaseConnection;
+import model.User;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
+    User save(User user) throws SQLException, IOException {
+        String sql = "INSERT INTO users (username, email) VALUES (?, ?)";
+        String username = user.
+        String email = user.
+
+        Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, email);
+
+        return user;
+    }
 }
